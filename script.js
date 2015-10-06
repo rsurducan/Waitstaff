0f80d73a797e85123fe08f80740d2a1843eb17d4
Radu Surducan <radu.surducan@autodesk.com>
2015-10-06 08:35:14 -0400

    I added validation

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 angular.module('myApp', [])
 .controller('myCtrl', function($scope) {
+    $scope.showErrors = true;
+
     $scope.mealDetails = {};
     $scope.customerCharges = {};
     $scope.earningsInfo = {};
 
     $scope.submit = function() {
-        $scope.customerCharges.subtotal = ($scope.mealDetails.baseMealPrice * (100 + $scope.mealDetails.taxRate)) / 100;
-        $scope.customerCharges.tip = ($scope.customerCharges.subtotal * $scope.mealDetails.tipPercentage) / 100;
-        $scope.customerCharges.total = $scope.customerCharges.subtotal + $scope.customerCharges.tip;
+        $scope.showErrors = true;
+        if ($scope.myForm.$valid) {
+            $scope.customerCharges.subtotal = ($scope.mealDetails.baseMealPrice * (100 + $scope.mealDetails.taxRate)) / 100;
+            $scope.customerCharges.tip = ($scope.customerCharges.subtotal * $scope.mealDetails.tipPercentage) / 100;
+            $scope.customerCharges.total = $scope.customerCharges.subtotal + $scope.customerCharges.tip;
 
-        $scope.earningsInfo.tipTotal = ($scope.earningsInfo.tipTotal || 0.00) + $scope.customerCharges.tip;
-        $scope.earningsInfo.mealCount = ($scope.earningsInfo.mealCount || 0) + 1;
-        $scope.earningsInfo.avgTip = ($scope.earningsInfo.tipTotal || 0) / ($scope.earningsInfo.mealCount || 1);
+            $scope.earningsInfo.tipTotal = ($scope.earningsInfo.tipTotal || 0.00) + $scope.customerCharges.tip;
+            $scope.earningsInfo.mealCount = ($scope.earningsInfo.mealCount || 0) + 1;
+            $scope.earningsInfo.avgTip = ($scope.earningsInfo.tipTotal || 0) / ($scope.earningsInfo.mealCount || 1);            
+        }
     };
 
     $scope.clearInput = function() {
         $scope.mealDetails = {};
+        $scope.showErrors = false;
     };
 
     $scope.reset = function() {
         $scope.mealDetails = {};
         $scope.customerCharges = {};
         $scope.earningsInfo = {};
+        $scope.showErrors = false;
     };
 
 });
