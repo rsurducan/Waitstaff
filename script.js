46d28f7ec2fdf4608fadcf0b8b55b394b08677a1
Radu Surducan <radu.surducan@autodesk.com>
2015-11-03 07:32:07 -0500

    I added animation to Waitstuff calculator

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ angular.module('myApp', ['ngRoute'])
             template : '<p>Error - Page Not Found</p>'
         }).otherwise('/error');
     }])
+    .run(function($rootScope, $location, $timeout) {
+        $rootScope.$on('$routeChangeError', function() {
+            $location.path("/error");
+        });
+        $rootScope.$on('$routeChangeStart', function() {
+            $rootScope.isLoading = true;
+        });
+        $rootScope.$on('$routeChangeSuccess', function() {
+            $timeout(function() {
+                $rootScope.isLoading = false;
+            }, 1000);
+        });
+    })
     .controller('HomeCtrl', function($scope) {
        
     })
